fc342cf751c4ac9672c70a6546ce0073668d5335
noproblematall <luisroyrsg@gmail.com>
2020-06-15 06:24:30 -0700

    some

--- app/Http/Controllers/API/InfoUpdateController.php
+++ app/Http/Controllers/API/InfoUpdateController.php
@@ -5,12 +5,43 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\EnergyLead;
+
 class InfoUpdateController extends Controller
 {
     
     public function energy_lead(Request $request)
     {
-        return response()->json($request->all());
+        $lead = new EnergyLead();
+        $lead->name  = $request->get('name');
+        $lead->email = $request->get('email');
+        $lead->phone = $request->get('phone');
+        $lead->comment = $request->get('comment');
+        $lead->zip_code = $request->get('zip');
+        if ($request->get('apartment') == true) {
+            $lead->home_type = 'apartment';
+        }else if ($request->get('house') == true) {
+            $lead->home_type = 'home';
+        }else{
+            $lead->home_type = '';
+        }
+        if ($request->get('landlord') == true) {
+            $lead->manager_type = 'landlord';
+        }else if ($request->get('tenant') == true) {
+            $lead->manager_type = 'tenant';
+        }else{
+            $lead->manager_type = '';
+        }
+        $lead->energy_type = $request->get('heating_value');
+        $lead->resident_number = $request->get('number_value');
+        $lead->isolate = $request->get('roof_value');
+        $lead->more_info = $request->get('more_value');
+        $lead->area = $request->get('area');
+        $lead->bill = $request->get('bill_value');
+        $lead->want_work = implode(',', $request->get('accom_value'));
+        $lead->save();
+
+        return response('ok', 200);
     }
 
     public function energy_contact(Request $request)
